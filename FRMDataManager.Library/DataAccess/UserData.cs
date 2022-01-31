c6371d0c06ff492dd0115203dfd6623c3df880df
byksy <ayisan1096@gmail.com>
2022-01-31 07:55:30 +0300

    Add User Control for Getting User Data

---
+++ FRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,23 @@
+﻿using FRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById (string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<UserModel,dynamic>("dbo.spUserLookup", p, "FRMData");
+
+            return output;
+        }
+    }
+}
